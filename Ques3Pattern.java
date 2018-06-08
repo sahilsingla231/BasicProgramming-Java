622224483426d26b167cc290066d55f9f22afca8
sahil <sahilsingla231231@gmail.com>
2018-06-08 13:44:52 +0530

    Qusetion 3 done

---
+++ Ques3Pattern.java
@@ -0,0 +1,19 @@
+
+public class Ques3Pattern {
+
+	public static void main(String[] args) {
+
+		int i,j;
+	
+		for(i=0;i<4;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print("*"+"");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
